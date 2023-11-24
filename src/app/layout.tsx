24bd019e67bf38edfbe9759c1548f1fdd2061c59
Morphisor <a.gallitri@gmail.com>
2023-11-24 09:19:19 +0100

    feat: added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { cx } from '../utils'
 import { Footer, Header } from '../components'
 import { SITE_METADATA } from '../utils/site-metadata'
 import { ThemeScript } from '../components/ThemeScript'
+import Script from "next/script"
 
 const inter = Inter({ subsets: ['latin'], display: 'swap', variable: '--font-in' })
 const manrope = Manrope({ subsets: ['latin'], display: 'swap', variable: '--font-mr' })
@@ -53,8 +54,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body 
-      className={cx(inter.variable, manrope.variable, 'font-mr bg-light dark:bg-dark')}>
+      <body className={cx(inter.variable, manrope.variable, 'font-mr bg-light dark:bg-dark')}>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-J05278L1KT"></Script>
+      <Script>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-J05278L1KT');
+        `}
+      </Script>
         <ThemeScript />
         <Header />
         {children}
